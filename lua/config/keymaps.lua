b5c3a677f5b64fbefbacb8c14c8a5746238a3b37
Rogerio Bispo <rogerio_pd@yahoo.com.br>
2025-02-25 12:05:42 -0300

    can debug on go

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+local dap = require("dap")
+
+map("n", "<F5>", dap.continue, { desc = "Start/Continue Debugging" })
+map("n", "<F10>", dap.step_over, { desc = "Step Over" })
+map("n", "<F11>", dap.step_into, { desc = "Step Into" })
+map("n", "<F12>", dap.step_out, { desc = "Step Out" })
+map("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+map("n", "<Leader>dB", function()
+  dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+end, { desc = "Set Conditional Breakpoint" })
---
+++ lua/plugins/dap-go.lua
@@ -0,0 +1,7 @@
+return {
+  "leoluz/nvim-dap-go",
+  dependencies = { "mfussenegger/nvim-dap" },
+  config = function()
+    require("dap-go").setup()
+  end,
+}
